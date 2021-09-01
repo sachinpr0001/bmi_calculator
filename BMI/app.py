a1c87236b2c706d3d28c5c94e57f4bb55b135349
sachinpr0001 <sachinpr0001@gmail.com>
2021-09-02 02:04:40 +0530

    reuploaded as a seperate repo

---
+++ BMI/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST']) 
+def index():
+    bmi = ''
+    if request.method == 'POST' and 'weight' in request.form:
+        weight = float(request.form.get('weight'))
+        height = float(request.form.get('height'))
+        bmi = calc_bmi(weight,height)
+    return render_template("bmi_calc.html",
+                            bmi=bmi)
+
+
+def calc_bmi(weight, height):
+    return round( (weight/ ( (height / 100) ** 2)), 2)
+
+
+app.run()
+
---
+++ Welcome/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST']) 
+def rootpage():
+    name = ''
+    food = ''
+    if request.method == 'POST' and 'username'in request.form:
+        name = request.form.get('username')
+        food = request.form.get('userfood')
+    return render_template("index.html",
+                            name=name,
+                            food = food)
+
+
+app.run()
+
